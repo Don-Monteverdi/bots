4dd4dbe9265261ecea77742204b56c31fa2bbdbf
Don-Monteverdi <zoldhegyi.g@gmail.com>
2025-06-27 10:46:03 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import re
 import os
 
 app = Flask(__name__, template_folder="templates")
-CORS(app)  # Enable CORS for all domains
+CORS(app)
 
 @app.route("/parse", methods=["POST"])
 def parse_pdf():
@@ -81,10 +81,10 @@ def parse_pdf():
         "Transactions": results
     })
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    @app.route("/", methods=["GET"])
+@app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=False, host="0.0.0.0", port=port)
